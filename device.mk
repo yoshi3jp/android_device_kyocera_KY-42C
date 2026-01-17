573fefd9f81ae3ceee196a138465eb17725eb768
Jamie Falconer <mrfluffyoven@gmail.com>
2026-01-17 07:05:36 +0000

    Add health service

--- device.mk
+++ device.mk
@@ -1,3 +1,7 @@
-# Reserved
-
 LOCAL_PATH := device/kyocera/KY41C
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-impl.recovery \
+    android.hardware.health@2.0-service
