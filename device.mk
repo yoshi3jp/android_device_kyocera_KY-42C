b8eee1dbcbc67940e5beb59dabcee723e6de4c5f
Jamie Falconer <mrfluffyoven@gmail.com>
2026-01-17 10:05:15 +0000

    Add API levels and VNDK version

--- device.mk
+++ device.mk
@@ -5,3 +5,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-impl.recovery \
     android.hardware.health@2.0-service
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 29
