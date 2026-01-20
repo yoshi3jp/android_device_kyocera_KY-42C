a53d6b47f529ea9ef4d33fbaf146d41d348d8781
Yoshinobu Date <82716291+yoshi3jp@users.noreply.github.com>
2026-01-20 17:57:16 +0900

    Add symlink for kmsetkey to default to Trustonic
    Set Trustonic as default via symlink for kmsetkey.

--- Android.mk
+++ Android.mk
@@ -7,6 +7,15 @@
 
 LOCAL_PATH := $(call my-dir)
 
+#SET TRUSTONIC TO DEFAULT VIA SYMLINK
+include $(CLEAR_VARS)
+LOCAL_MODULE := kmsetkey_symlinks
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_TAGS := optional
+LOCAL_POST_INSTALL_CMD := \
+    ln -sf /vendor/lib/hw/kmsetkey.trustonic.so $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.default.so;
+include $(BUILD_SYSTEM)/base_rules.mk
+
 ifeq ($(TARGET_DEVICE),KY41C)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
