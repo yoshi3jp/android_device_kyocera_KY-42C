57522651d63292b71173633f7e0b56a44bf89a50
Yoshinobu Date <82716291+yoshi3jp@users.noreply.github.com>
2026-01-20 14:24:45 +0900

    Specify trustonic for keymaster and gatekeeper

--- device.mk
+++ device.mk
@@ -19,6 +19,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.keymaster.version=3.0 \
- 	ro.hardware.keystore_desede=true \
- 	ro.hardware.keystore=software \
- 	ro.hardware.gatekeeper=software
+    ro.hardware.kmsetkey=trustonic \ 
+    ro.hardware.gatekeeper=trustonic \ 
+    ro.vendor.mtk_trustonic_tee_support=1
+
+
