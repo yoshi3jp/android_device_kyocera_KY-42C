154eaabbaf7bcc0c1caf8aab2d7157fa4b297dd7
Yoshinobu Date <82716291+yoshi3jp@users.noreply.github.com>
2026-01-21 17:34:30 +0900

    Filter out vibrator service from recovery configuration
    Exclude vibrator service and related libraries from recovery modules and packages. Increasingly explicit.

--- twrp_KY41C.mk
+++ twrp_KY41C.mk
@@ -31,4 +31,22 @@ PRODUCT_PACKAGES_REMOVE := \
     vendor.qti.hardware.vibrator.impl \
     libqtivibratoreffect
 
+TARGET_RECOVERY_DEVICE_MODULES := $(filter-out \
+    vendor.qti.hardware.vibrator.service \
+    vendor.qti.hardware.vibrator.impl \
+    libqtivibratoreffect, \
+    $(TARGET_RECOVERY_DEVICE_MODULES))
+
+RECOVERY_LIBRARY_SOURCE_FILES := $(filter-out \
+    %/hw/vendor.qti.hardware.vibrator.service \
+    %/vendor.qti.hardware.vibrator.impl.so \
+    %/libqtivibratoreffect.so, \
+    $(RECOVERY_LIBRARY_SOURCE_FILES))
+
+PRODUCT_PACKAGES := $(filter-out \
+    vendor.qti.hardware.vibrator.service \
+    vendor.qti.hardware.vibrator.impl \
+    libqtivibratoreffect, \
+    $(PRODUCT_PACKAGES))
+
 BUILD_FINGERPRINT := KYOCERA/KY-41C/KY-41C:10/1.060DE.0176.a/1.060DE.0176.a:user/release-keys
