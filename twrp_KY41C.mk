6f4f44059e8bf5b1877bc2a51f3ab2d031d334c5
Yoshinobu Date <82716291+yoshi3jp@users.noreply.github.com>
2026-01-22 13:22:08 +0900

    Remove the suppression of Qualcomm vibrator modules
    Specifying VNDK = 29 was the cause of this issue. It should not be needed now, as it has been reverted.

--- twrp_KY41C.mk
+++ twrp_KY41C.mk
@@ -25,30 +25,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-kyocera
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="KY-41C-user 10 1.060DE.0176.a 1.060DE.0176.a release-keys"
 
-# --- MTK build: explicitly drop Qualcomm/QTI vibrator modules from recovery ---
-PRODUCT_PACKAGES_REMOVE := \
-    vendor.qti.hardware.vibrator.service \
-    vendor.qti.hardware.vibrator.impl \
-    libqtivibratoreffect
-
-TARGET_RECOVERY_DEVICE_MODULES := $(filter-out \
-    vendor.qti.hardware.vibrator.service \
-    vendor.qti.hardware.vibrator.impl \
-    libqtivibratoreffect, \
-    $(TARGET_RECOVERY_DEVICE_MODULES))
-
-RECOVERY_LIBRARY_SOURCE_FILES := $(filter-out \
-    %/hw/vendor.qti.hardware.vibrator.service \
-    %/vendor.qti.hardware.vibrator.impl.so \
-    %/libqtivibratoreffect.so, \
-    $(RECOVERY_LIBRARY_SOURCE_FILES))
-
-PRODUCT_PACKAGES := $(filter-out \
-    vendor.qti.hardware.vibrator.service \
-    vendor.qti.hardware.vibrator.impl \
-    libqtivibratoreffect, \
-    $(PRODUCT_PACKAGES))
-
-PRODUCT_SOONG_NAMESPACES := $(filter-out vendor/qcom% hardware/qcom%,$(PRODUCT_SOONG_NAMESPACES))
-
 BUILD_FINGERPRINT := KYOCERA/KY-41C/KY-41C:10/1.060DE.0176.a/1.060DE.0176.a:user/release-keys
