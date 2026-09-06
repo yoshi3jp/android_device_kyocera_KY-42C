26198ba1c7eb173063d80f87144a222332aeed19
Yoshinobu Date <iam@yoshinobu.date>
2026-09-07 01:59:37 +0900

    twrp: ky42c: configure pure ARM64 FBE recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,20 +12,13 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 #
-# Primary native recovery userspace
+# Native ARM64 recovery userspace
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
-
-# Secondary compatibility userspace
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
 
 TARGET_BOARD_SUFFIX := _64
 TARGET_IS_64_BIT := true
